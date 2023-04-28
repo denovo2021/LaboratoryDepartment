0bee32be48eca84dfb886abe0c4270ab926afca6
TK <tk@MacBook-Pro-5.local>
2023-04-28 21:46:28 +0900

    Uploaded

--- script.js
+++ script.js
@@ -1,74 +1,57 @@
-// 問題データと解答データのサンプル
 const questions = [
-  {
-    text: '問題1: 医療情報システムで臨床検査項目分類コードとして用いられるのはどれか。',
-    choices: ['1. DICOM', '2. HL7', '3. ICD11', '4. JLAC', '5. PACS'],
-    correctAnswer: 4,
-  },
-  {
-    text: '問題2: 波長220nmの光の分類はどれか。',
-    choices: ['1. 紫外線A', '2. 紫外線B', '3. 紫外線C', '4. 赤外線A', '5. 赤外線B'],
-    correctAnswer: 3,
-  },
-  // 他の問題を追加
+  // ここに質問データを追加
 ];
 
-let currentQuestionIndex = 0;
-let userAnswer = null;
+const answers = [
+  // ここに解答データを追加
+];
 
-function displayQuestion() {
-  const question = questions[currentQuestionIndex];
-  document.getElementById('question-text').innerText = question.text;
-  const choices = document.querySelectorAll('.choice');
-  for (let i = 0; i < choices.length; i++) {
-    choices[i].innerText = question.choices[i];
-  }
+function createQuestionElement(question, index) {
+  const questionElement = document.createElement("div");
+  questionElement.classList.add("question");
+
+  const questionTitle = document.createElement("h3");
+  questionTitle.textContent = `問${index + 1}: ${question.title}`;
+  questionElement.appendChild(questionTitle);
+
+  question.choices.forEach((choice, i) => {
+    const choiceElement = document.createElement("div");
+    choiceElement.classList.add("choice");
+    choiceElement.textContent = `${i + 1}. ${choice}`;
+
+    choiceElement.addEventListener("click", (event) => {
+      handleChoiceClick(event, index);
+    });
+
+    questionElement.appendChild(choiceElement);
+  });
+
+  return questionElement;
 }
 
-document.querySelectorAll('.choice').forEach((choice) => {
-  choice.addEventListener('click', (e) => {
-    userAnswer = parseInt(e.target.dataset.choice);
-  });
-});
-
-document.getElementById('submit-answer').addEventListener('click', () => {
-  const resultText = document.getElementById('result-text');
-  if (userAnswer === questions[currentQuestionIndex].correctAnswer) {
-    resultText.innerText = '正解！';
-  } else {
-    resultText.innerText = '不正解...';
-  }
-  document.getElementById('question-container').style.display = 'none';
-  document.getElementById('result-container').style.display = 'block';
-});
-
-document.getElementById('next-question').addEventListener('click', () => {
-  currentQuestionIndex++;
-  if (currentQuestionIndex < questions.length) {
-    displayQuestion();
-    document.getElementById('question-container').style.display = 'block';
-    document.getElementById('result-container').style.display = 'none';
-  } else {
-    alert('すべての問題が終了しました！');
-  }
-});
-
-// ...
-
-function handleChoiceClick(event) {
-  // 選択済みの選択肢の色をリセットする
+function handleChoiceClick(event, questionIndex) {
   const selectedChoices = document.querySelectorAll(".choice.selected");
   selectedChoices.forEach((choice) => {
     choice.classList.remove("selected");
   });
 
-  // クリックされた選択肢に 'selected' クラスを追加
   event.target.classList.add("selected");
-
-  // ...
+  showAnswer(questionIndex, event.target.textContent.charAt(0));
 }
 
-// ...
+function showAnswer(questionIndex, userChoice) {
+  const answer = answers[questionIndex];
+  const message = userChoice == answer ? "正解です！" : `不正解。正解は${answer}です。`;
+  alert(message);
+}
 
+function renderQuestions() {
+  const container = document.getElementById("questions-container");
 
-displayQuestion();
+  questions.forEach((question, index) => {
+    const questionElement = createQuestionElement(question, index);
+    container.appendChild(questionElement);
+  });
+}
+
+renderQuestions();
