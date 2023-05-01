f72396e8eff44414662aab5cf60a6b4b67f835c6
TK <tk@MacBook-Pro-5.local>
2023-05-01 17:42:41 +0900

    Uploaded

---
+++ main.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>クイズアプリ</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div id="app">
+    <h1>臨床検査技師国家試験問題</h1>
+    <ul>
+      <li><a id="link-am">068午前</a></li>
+      <li><a id="link-pm">068午後</a></li>
+    </ul>
+    <div id="question"></div>
+    <ol id="choices"></ol>
+    <div id="correct-answer"></div>
+  </div>
+  <script src="main.js"></script>
+</body>
+</html>
