3976ee1f9df4614e5cd869083768e1b1a20c3c77
TK <tk@MacBook-Pro-5.local>
2023-05-06 11:01:02 +0900

    Uploaded

--- main.js
+++ main.js
@@ -1,64 +1,53 @@
 const questionSetA = "068/question_068_01.json";
 const questionSetB = "068/question_068_02.json";
 const answerSet = "068/ans_068.json";
+
 let questionsA = [];
 let questionsB = [];
 let answers = {};
-let currentQuestion;
+
+let currentQuestion = null;
 let selectedIndices = [];
 
-fetch(questionSetA)
-  .then((response) => response.json())
-  .then((data) => {
-    questionsA = data;
-  });
-
-fetch(questionSetB)
-  .then((response) => response.json())
-  .then((data) => {
-    questionsB = data;
-  });
-
-fetch(answerSet)
-  .then((response) => response.json())
-  .then((data) => {
-    answers = data;
-  });
+function getRandomQuestion() {
+  const questionSet = Math.random() < 0.5 ? questionsA : questionsB;
+  return questionSet[Math.floor(Math.random() * questionSet.length)];
+}
 
 function loadQuestion() {
-  const questionSet = Math.random() < 0.5 ? questionsA : questionsB;
-  const question = questionSet[Math.floor(Math.random() * questionSet.length)];
-  currentQuestion = question;
-
-  $("#question-text").text(question.text);
+  currentQuestion = getRandomQuestion();
+  $("#question-text").text(currentQuestion.text);
   $("#choices").empty();
 
-  question.choices.forEach((choice, index) => {
-    const choiceBtn = $("<button>").addClass("btn btn-outline-primary").text(choice);
-    choiceBtn.on("click", function() {
-      $(this).toggleClass("btn-outline-primary btn-primary");
-      toggleChoice(index);
-    });
-    $("#choices").append(choiceBtn);
+  currentQuestion.choices.forEach((choice, index) => {
+    const choiceElement = $("<button>")
+      .addClass("choice")
+      .text(choice)
+      .on("click", function () {
+        const selectedIndex = selectedIndices.indexOf(index);
+
+        if (selectedIndex === -1) {
+          selectedIndices.push(index);
+          $(this).addClass("selected");
+        } else {
+          selectedIndices.splice(selectedIndex, 1);
+          $(this).removeClass("selected");
+        }
+      });
+
+    $("#choices").append(choiceElement);
   });
 }
 
-function toggleChoice(index) {
-  if (selectedIndices.includes(index)) {
-    selectedIndices = selectedIndices.filter((i) => i !== index);
-  } else {
-    selectedIndices.push(index);
-  }
-}
-
-// checkAnswer function (continuation)
 function checkAnswer() {
   const answerKey = currentQuestion.number;
   const answerValue = answers["A" + answerKey];
   const correctAnswer = answerValue.toString().split("").map(Number);
 
-  if (selectedIndices.length === correctAnswer.length &&
-      selectedIndices.every((index) => correctAnswer.includes(index + 1))) {
+  if (
+    selectedIndices.length === correctAnswer.length &&
+    selectedIndices.every((index) => correctAnswer.includes(index + 1))
+  ) {
     alert("正解!");
   } else {
     alert("不正解。正解は: " + correctAnswer.join(", "));
@@ -67,9 +56,27 @@ function checkAnswer() {
   loadQuestion();
 }
 
-$("#submit-answer").on("click", function() {
+$("#submit-answer").on("click", function () {
   checkAnswer();
   selectedIndices = [];
 });
 
-loadQuestion();
+Promise.all([
+  fetch(questionSetA)
+    .then((response) => response.json())
+    .then((data) => {
+      questionsA = data;
+    }),
+  fetch(questionSetB)
+    .then((response) => response.json())
+    .then((data) => {
+      questionsB = data;
+    }),
+  fetch(answerSet)
+    .then((response) => response.json())
+    .then((data) => {
+      answers = data;
+    }),
+]).then(() => {
+  loadQuestion();
+});
